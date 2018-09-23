711c9c8f0fe3f3e0c4e18cfc2fbcc7915f4c57f7
Belen <>
2018-09-23 13:57:04 +0200

    done muy sucio, fallo en la redirección del id

---
+++ billboard/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MovieComponent } from './components/movie/movie.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path:'movie/:id', component: MovieComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ billboard/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MovieItemComponent } from './components/movie-item/movie-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MovieComponent,
+    MenuComponent,
+    MovieItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
